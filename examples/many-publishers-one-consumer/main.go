fdcddbe34f241e43ce9573a9e298942817375e9f
Chris Howell <crhowell@users.noreply.github.com>
2023-12-18 03:49:27 -0800

    WIP: complete refactor of intracom (mutex based) to lock-free channel based approach

---
+++ examples/many-publishers-one-consumer/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/ambitiousfew/intracom"
+)
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	ic := intracom.New[string](ctx)
+
+	topic := "metric-events"
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+	// Register topic, receive a publishing channel
+
+	publishCh, unregister := ic.Register(topic)
+	publisherCount := 5
+
+	go func() {
+		defer wg.Done()
+		defer unregister()
+
+		// spawn routine that will launch 5 publishers
+		var pwg sync.WaitGroup
+		pwg.Add(publisherCount)
+		for i := 0; i < publisherCount; i++ { // spawn 5 publishers
+			i := i
+			go func() {
+				defer pwg.Done()
+				publishCh <- fmt.Sprintf("Hello from publisher %d", i+1)
+			}()
+		}
+		pwg.Wait()
+
+	}()
+
+	// launch consumer that prints messages
+	go func() {
+		defer wg.Done()
+		// Create a consumer
+		consumer, unsubscribe := ic.Subscribe(&intracom.ConsumerConfig{
+			Topic:         topic,
+			ConsumerGroup: "consumer1",
+			BufferSize:    100,
+			BufferPolicy:  intracom.DropNone,
+		})
+
+		defer unsubscribe()
+
+		for msg := range consumer {
+			fmt.Println(msg)
+		}
+	}()
+
+	// wait for all routines to finish
+	wg.Wait()
+	fmt.Println("done")
+}
---
+++ examples/one-publisher-many-consumers/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/ambitiousfew/intracom"
+)
+
+func startPublisher(publishC chan<- string) {
+	// time.Sleep(2 * time.Second)
+	// publish a message 10 times.
+	for i := 0; i < 10; i++ {
+		publishC <- fmt.Sprintf("message %d", i)
+	}
+}
+
+func main() {
+	// a parent context that can be cancelled to end all underlying channels inside Intracom
+	// cleanup performed, Intracom instance is no longer usable after cancel
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	ic := intracom.New[string](ctx)
+
+	topic := "events"
+
+	// use .Register to register a topic you wish to publish to.
+	// you will receive a send-only channel for your producers
+	// and a bound-callable function that when called will unregister this topic.
+	publishCh, unregister := ic.Register(topic)
+
+	// For this example we are launching 3 routines
+	// 2 routines that will subscribe to the same topic with different consumer names.
+	// 1 routine that will publish to the same topic.
+	// Publishes will be "broadcasted" to all subscribers of the same topic.
+	// Late subscribers will only receive messages from the time at which they connect.
+	var wg sync.WaitGroup
+	wg.Add(3)
+
+	go func() { // consumer1 subscribing using intracom instance.
+		defer wg.Done()
+		// consumer 1
+		ch, _ := ic.Subscribe(&intracom.ConsumerConfig{
+			Topic:         topic,
+			ConsumerGroup: "consumer1",
+			BufferSize:    10,
+			BufferPolicy:  intracom.DropNone,
+		})
+
+		for message := range ch {
+			fmt.Println("consumer1 message received: ", message)
+		}
+	}()
+
+	go func() { // consumer2 subscribing using intracom instance.
+		defer wg.Done()
+		// consumer 2
+		ch, _ := ic.Subscribe(&intracom.ConsumerConfig{
+			Topic:         topic,
+			ConsumerGroup: "consumer2",
+			BufferSize:    10,
+			BufferPolicy:  intracom.DropNone,
+		})
+
+		for message := range ch {
+			fmt.Println("consumer2 message received: ", message)
+		}
+	}()
+
+	go func() { // publisher of 10 messages, 0 through 9
+		defer wg.Done()
+		defer unregister() // nothing more will be produced, unregister topic.
+		startPublisher(publishCh)
+	}()
+
+	fmt.Println("waiting for metric events consumer")
+	// consume from a receive-only channel that publisher pushes into.
+	wg.Wait()
+	fmt.Println("done.")
+}
